28e3cbb4787bc49f34318d3038b6b96d136a205e
yujunpapa <pa324@naver.com>
2024-11-18 21:52:41 +0900

    controller,service 패키지 분리

--- src/main/kotlin/kr/flab/f5/f5template/controller/ProductController.kt
+++ src/main/kotlin/kr/flab/f5/f5template/controller/ProductController.kt
@@ -1,8 +1,11 @@
-package kr.flab.f5.f5template.application
+package kr.flab.f5.f5template.controller
 
 import io.swagger.annotations.Api
+import kr.flab.f5.f5template.controller.request.DecreaseStockRequest
+import kr.flab.f5.f5template.service.ProductService
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,7 +18,8 @@ class ProductController(
 
     @PatchMapping("/{id}/stock")
     fun decreaseStock(
-        @PathVariable id: Long
+        @PathVariable id: Long,
+        @RequestBody request: DecreaseStockRequest
     ) {
         productService.decreaseStock(id)
     }
