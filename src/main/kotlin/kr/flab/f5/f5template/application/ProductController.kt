f3560ff3ef6c87c3c00bdc845072d60e02388606
acecic82 <102938al@naver.com>
2024-11-18 17:13:44 +0900

    수정 (ProductController class)
    수정 내용
    - 삭제 api 추가
    - 생성 Api 추가
    - 수정 api 추가
    - 조회 api 추가

--- src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
+++ src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
@@ -1,9 +1,16 @@
 package kr.flab.f5.f5template.application
 
 import io.swagger.annotations.Api
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @Api(tags = ["Product"])
@@ -18,5 +25,35 @@ class ProductController(
         @PathVariable id: Long
     ) {
         productService.decreaseStock(id)
+    }
+
+    @GetMapping("/{id}")
+    fun findProduct(
+        @PathVariable id: Long
+    ): ProductDTO {
+        return ProductDTO.from(productService.findProduct(id))
+    }
+
+    @PutMapping("/{id}")
+    fun updateProduct(
+        @PathVariable id: Long,
+        @RequestBody product: ProductDTO
+    ) {
+        productService.updateProduct(id, product.toVO())
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createProduct(
+        @RequestBody product: ProductDTO
+    ) {
+        productService.createProduct(product.toVO())
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteProduct(
+        @PathVariable id: Long
+    ) {
+        productService.deleteProduct(id)
     }
 }
