324a406e97b2b8203ea0836949216d7450a86b55
kimich1218 <ickim1218@gmail.com>
2024-11-21 16:41:19 +0900

    feat: 상품등록 controller 구현

--- src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
+++ src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
@@ -1,8 +1,13 @@
 package kr.flab.f5.f5template.application
 
 import io.swagger.annotations.Api
+import kr.flab.f5.f5template.result.ResultCode.*
+import kr.flab.f5.f5template.result.ResultResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,5 +23,11 @@ class ProductController(
         @PathVariable id: Long
     ) {
         productService.decreaseStock(id)
+    }
+
+    @PostMapping("/create")
+    fun createProduct(@RequestBody productCreateRequest: ProductCreateRequest): ResponseEntity<ResultResponse> {
+        productService.createStock(productCreateRequest)
+        return ResponseEntity.ok(ResultResponse.of(PRODUCT_CREATE_SUCCESS))
     }
 }
