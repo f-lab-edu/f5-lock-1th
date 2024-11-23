438f6a2d869c83d4134ed274ab37f83f59f1a84c
yujunpapa <pa324@naver.com>
2024-11-23 16:28:31 +0900

    삭제,조회 API 추가

--- src/main/kotlin/kr/flab/f5/f5template/controller/response/ProductResult.kt
+++ src/main/kotlin/kr/flab/f5/f5template/controller/response/ProductResult.kt
@@ -1,6 +1,11 @@
 package kr.flab.f5.f5template.controller.response
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ProductResult(
     val id: Long,
     val name: String,
+    val stock: Long? = null,
+    val price: Long? = null
 )
