b6d51f7aab420ab1770f15013c8b8a165628bbc0
kimich1218 <ickim1218@gmail.com>
2024-11-23 11:35:57 +0900

    feat: 상품 조회 controller 구현

--- src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
+++ src/main/kotlin/kr/flab/f5/f5template/application/ProductController.kt
@@ -1,9 +1,11 @@
 package kr.flab.f5.f5template.application
 
 import io.swagger.annotations.Api
+import kr.flab.f5.f5template.application.dto.ProductCreateRequest
 import kr.flab.f5.f5template.result.ResultCode.*
 import kr.flab.f5.f5template.result.ResultResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -29,5 +31,11 @@ class ProductController(
     fun createProduct(@RequestBody productCreateRequest: ProductCreateRequest): ResponseEntity<ResultResponse> {
         productService.createStock(productCreateRequest)
         return ResponseEntity.ok(ResultResponse.of(PRODUCT_CREATE_SUCCESS))
+    }
+
+    @GetMapping()
+    fun findAllProducts(): ResponseEntity<ResultResponse> {
+        val findAllProducts = productService.findAllProducts()
+        return ResponseEntity.ok(ResultResponse.of(PRODUCT_FIND_SUCCESS, findAllProducts))
     }
 }
