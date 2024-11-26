49dddb73e0b2e9bc9c727f0fc2ea5421cb3fd311
Wanni <wannidev0928@gmail.com>
2024-11-26 17:53:42 +0900

    재고 등록에 대한 동시성을 제어하기위해 stock 해시맵 구현체를 ConcurrentHashMap으로 변경

--- src/main/kotlin/kr/flab/f5/f5template/application/ProductService.kt
+++ src/main/kotlin/kr/flab/f5/f5template/application/ProductService.kt
@@ -2,6 +2,7 @@ package kr.flab.f5.f5template.application
 
 import kr.flab.f5.f5template.mysql.jpa.repository.ProductRepository
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
 
 const val DEFAULT_STOCK = 1000000
@@ -11,7 +12,7 @@ class ProductService(
     // 데이터베이스 없이 순수 자바로 동시성 이슈를 다뤄보기 위해 아직은 데이터베이스를 사용하지 않습니다.
     private val productRepository: ProductRepository,
 ) {
-    private val stock = HashMap<Long, Int>()
+    private val stock = ConcurrentHashMap<Long, Int>()
     private lateinit var id: AtomicLong
 
     init {
