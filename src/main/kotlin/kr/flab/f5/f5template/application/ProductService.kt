325fe306050b4e1a49684fd334c3cb6e447cfea4
Wanni <wannidev0928@gmail.com>
2024-11-26 16:53:25 +0900

    재고 검색 구현중 발견된 오류 수정
    재고 생성시 인메모리 db(해시맵)의 식별자 id가 4가 나오는 것을 기대했으나 0이 나오고 있었음. 클래스가 인스턴스화될 때 init 블록 내부의 코드가 실행된 직후에 id 멤버변수가 초기화될 것을 기대했으나,
    그게 아니었음을 발견하고 해당사항 수정

--- src/main/kotlin/kr/flab/f5/f5template/application/ProductService.kt
+++ src/main/kotlin/kr/flab/f5/f5template/application/ProductService.kt
@@ -6,18 +6,21 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicLong
 
+const val DEFAULT_STOCK = 1000000
+
 @Service
 class ProductService(
     // 데이터베이스 없이 순수 자바로 동시성 이슈를 다뤄보기 위해 아직은 데이터베이스를 사용하지 않습니다.
     private val productRepository: ProductRepository,
 ) {
     private val stock = HashMap<Long, Int>()
-    private val id = AtomicLong(stock.size.toLong())
+    private lateinit var id: AtomicLong
 
     init {
-        stock[1] = 1000000
-        stock[2] = 1000000
-        stock[3] = 1000000
+        stock[1] = DEFAULT_STOCK
+        stock[2] = DEFAULT_STOCK
+        stock[3] = DEFAULT_STOCK
+        id = AtomicLong(stock.size.toLong())
     }
 
     fun create(quantity: Int) {
@@ -31,6 +34,8 @@ class ProductService(
         }
         stock[id] = currentStock - 1
     }
+
+    fun searchStock(id: Long): Int = stock[id] ?: throw IllegalArgumentException("No stock for product $id")
 }
 
 fun main() {
