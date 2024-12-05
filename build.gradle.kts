9a9e7348a74b39fd919fdcf3e66fa64def31e625
yujunpapa <pa324@naver.com>
2024-12-05 22:44:20 +0900

    jdk 1.9호환 가능하도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "2.5.5"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("plugin.jpa") version "1.9.25"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
-
 }
 
 group = "kr.flab.f5"
@@ -49,6 +48,9 @@ dependencies {
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
