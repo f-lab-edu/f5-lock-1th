d7e27737c03860c96f1772f4baacbb67ff55643c
yujunpapa <pa324@naver.com>
2024-12-06 13:12:12 +0900

    java 11 rollback

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
@@ -44,7 +44,7 @@ kotlin {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
