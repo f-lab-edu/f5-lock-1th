b8733b9a07dcdc8ca5d667deed439afea696fd22
kimich1218 <ickim1218@gmail.com>
2024-12-13 16:22:36 +0900

    feat: 노베이스로 ConcurrentHashMap 구현

--- src/main/kotlin/kr/flab/f5/f5template/lecture/week2/Lecture1HashMap.kt
+++ src/main/kotlin/kr/flab/f5/f5template/lecture/week2/Lecture1HashMap.kt
@@ -1,60 +1,87 @@
 package kr.flab.f5.f5template.lecture.week2
 
+import java.util.concurrent.atomic.AtomicReference
+
 class Lecture1HashMap<K, V> : java.util.Map<K, V> {
 
-    private val innerMap = HashMap<K, V>()
+    private val innerMap = AtomicReference(HashMap<K, V>())
 
     override fun get(key: Any?): V? {
-        return innerMap[key]
+        return innerMap.get()[key]
     }
 
     override fun put(key: K, value: V): V? {
-        return innerMap.put(key, value)
+        while (true) {
+            val currentMap = innerMap.get()
+            val newMap = HashMap(currentMap)
+            newMap[key] = value
+            if (innerMap.compareAndSet(currentMap, newMap)) {
+                return currentMap[key]
+            }
+        }
     }
 
     override fun remove(key: Any?): V? {
-        return innerMap.remove(key)
+        while (true) {
+            val currentMap = innerMap.get()
+            val newMap = HashMap(currentMap)
+            newMap.remove(key)
+            if (innerMap.compareAndSet(currentMap, newMap)) {
+                return currentMap[key]
+            }
+        }
     }
 
     override fun putIfAbsent(key: K, value: V): V? {
-        return innerMap.putIfAbsent(key, value)
+        while (true) {
+            val currentMap = innerMap.get()
+            val newMap = HashMap(currentMap)
+            if (!newMap.containsKey(key)) {
+                newMap[key] = value
+                if (innerMap.compareAndSet(currentMap, newMap)) {
+                    return null
+                }
+            } else {
+                return currentMap[key]
+            }
+        }
     }
 
     override fun size(): Int {
-        return innerMap.size
+        return innerMap.get().size
     }
 
     override fun isEmpty(): Boolean {
-        return innerMap.isEmpty()
+        return innerMap.get().isEmpty()
     }
 
     // ------- 이 아래는 구현하지 않으셔도 됩니다 ----------
 
     override fun containsKey(key: Any?): Boolean {
-        return innerMap.containsKey(key)
+        return innerMap.get().containsKey(key)
     }
 
     override fun containsValue(value: Any?): Boolean {
-        return innerMap.containsValue(value)
+        return innerMap.get().containsValue(value)
     }
 
     override fun clear() {
-        innerMap.clear()
+        innerMap.get().clear()
     }
 
     override fun keySet(): MutableSet<K> {
-        return innerMap.keys
+        return innerMap.get().keys
     }
 
     override fun values(): MutableCollection<V> {
-        return innerMap.values
+        return innerMap.get().values
     }
 
     override fun entrySet(): MutableSet<MutableMap.MutableEntry<K, V>> {
-        return innerMap.entries
+        return innerMap.get().entries
     }
 
     override fun putAll(m: MutableMap<out K, out V>) {
-        innerMap.putAll(m)
+        innerMap.get().putAll(m)
     }
 }
