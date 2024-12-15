28b612d9419d77ba0d6e99a9d23d5df011aaa1bb
acecic82 <102938al@naver.com>
2024-12-15 13:56:45 +0900

    수정 (Singleton Test 추가)
    수정 내용
    - Semaphore 로 만들어보기
    - CycleBarrier 로 만들어보기
    
    질문
    - 가끔 count 가 보장이 안되던데 왜 그럴까요?

--- src/test/kotlin/kr/flab/f5/f5template/lecture/week2/Example4SingletonTest.kt
+++ src/test/kotlin/kr/flab/f5/f5template/lecture/week2/Example4SingletonTest.kt
@@ -1,11 +1,11 @@
 package kr.flab.f5.f5template.lecture.week2
 
 import org.assertj.core.api.Assertions.assertThat
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.CyclicBarrier
 import java.util.concurrent.Executors
+import java.util.concurrent.Semaphore
+import kotlin.concurrent.thread
 import kotlin.test.Test
 
 class Example4SingletonTest {
@@ -13,22 +13,80 @@ class Example4SingletonTest {
     private val executor = Executors.newFixedThreadPool(100)
 
     @Test
-    fun `싱글톤은 1개의 객체만 만들어져야 한다`() {
+    fun `싱글톤은 1개의 객체만 만들어져야 한다 CountDownLatch Version`() {
         val n = 1000
-        val latch = CountDownLatch(1)
+        val latch = CountDownLatch(1000)
         val futures = (1..n).map {
             executor.submit<Example4Singleton> {
-                latch.await()
+                latch.countDown()
                 Example4Singleton.getInstance()
             }
         }
 
-        latch.countDown()
+        latch.await()
 
         val objects = futures.map {
             it.get()
         }.distinct()
 
         assertThat(objects.size).isEqualTo(1)
+    }
+
+    @Test
+    fun `싱글톤은 1개의 객체만 만들어져야 한다 Semaphore Version`() {
+        val n = 100
+        val semaphore = Semaphore(10)
+        var count = 0
+        val singletons = mutableListOf<Example4Singleton>()
+        val workers = mutableListOf<Thread>()
+
+        repeat(n) {id ->
+            val worker = thread {
+                semaphore.acquire()
+                singletons.add(Example4Singleton.getInstance())
+                println("Working acquire $id")
+                count++
+                Thread.sleep(10)
+                semaphore.release()
+                println("Working release $id")
+            }
+            workers.add(worker)
+        }
+
+        workers.forEach { it.join() }
+
+        val objects = singletons.distinct()
+
+        assertThat(objects.size).isEqualTo(1)
+        assertThat(count).isEqualTo(n)
+    }
+
+    @Test
+    fun `싱글톤은 1개의 객체만 만들어져야 한다 CycleBarrier Version`() {
+        val n = 1000
+        val singletons = mutableListOf<Example4Singleton>()
+        val workers = mutableListOf<Thread>()
+        var count = 0
+
+        val cycleBarrier = CyclicBarrier(n) {
+            println("All cycleBarrier is ended")
+        }
+
+        repeat(n) { id ->
+            val worker = thread {
+                singletons.add(Example4Singleton.getInstance())
+                count++
+                cycleBarrier.await() // 바리어에 도착
+            }
+            workers.add(worker)
+        }
+
+
+        workers.forEach { it.join() }
+        println("All workers wait here")
+        val objects = singletons.distinct()
+
+        assertThat(objects.size).isEqualTo(1)
+        assertThat(count).isEqualTo(n)
     }
 }
