6765734787b1a24e8ce622656d5f2e4947ebfaca
yujunpapa <pa324@naver.com>
2024-12-21 21:07:17 +0900

    스핀락으로 ConcurrentHashMap구현

--- src/main/kotlin/kr/flab/f5/f5template/lecture/week2/Lecture1HashMap.kt
+++ src/main/kotlin/kr/flab/f5/f5template/lecture/week2/Lecture1HashMap.kt
@@ -1,31 +1,57 @@
 package kr.flab.f5.f5template.lecture.week2
 
-class Lecture1HashMap<K, V> : java.util.Map<K, V> {
 
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
+
+class Lecture1HashMap<K, V> : java.util.Map<K, V> {
     private val innerMap = HashMap<K, V>()
+    private val atomicCounter = AtomicInteger(0)
+    private val spinLock = SpinLockUtil.getInstance<K>()
+
 
     override fun get(key: Any?): V? {
-        return innerMap[key]
+        return spinLock.withLock(key) {
+            innerMap[key]
+        }
     }
 
     override fun put(key: K, value: V): V? {
-        return innerMap.put(key, value)
+        return spinLock.withLock(key) {
+            val result = innerMap.put(key, value)
+            if (result == null) {
+                atomicCounter.incrementAndGet()
+            }
+            result
+        }
     }
 
     override fun remove(key: Any?): V? {
-        return innerMap.remove(key)
+        return spinLock.withLock(key) {
+            val result = innerMap.remove(key)
+            if (result != null) {
+                atomicCounter.decrementAndGet()
+            }
+            result
+        }
     }
 
     override fun putIfAbsent(key: K, value: V): V? {
-        return innerMap.putIfAbsent(key, value)
+        return spinLock.withLock(key) {
+            val result = innerMap.putIfAbsent(key, value)
+            if (result == null) {
+                atomicCounter.incrementAndGet()
+            }
+            result
+        }
     }
 
     override fun size(): Int {
-        return innerMap.size
+        return atomicCounter.get()
     }
 
     override fun isEmpty(): Boolean {
-        return innerMap.isEmpty()
+       return atomicCounter.get() == 0
     }
 
     // ------- 이 아래는 구현하지 않으셔도 됩니다 ----------
